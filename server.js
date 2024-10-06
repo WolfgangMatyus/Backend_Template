bb45c984e3b8e394b95c09734815f736cef07f13
WolfgangMatyus <wi21b055@technikum-wien.at>
2024-10-06 11:42:38 +0200

    server.js angepasst

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const membersRoute = require('../Backend_Template/project-root/src/routes/members');
+const authRoutes = require('../Backend_Template/project-root/src/routes/authRoutes'); // Importiere die Authentifizierungsrouten
 const app = express();
 
 app.use(bodyParser.json());
 
+// Registriere die Mitglieder-Routen
 app.use('/api/v1/members', membersRoute);
+
+// Registriere die Authentifizierungs-Routen
+app.use('/api/v1/auth', authRoutes); // Füge die Authentifizierungs-Route hinzu
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
